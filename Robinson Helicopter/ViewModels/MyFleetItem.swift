3c3854f6dda090e7e88f1085a182340ff4c37b5f
mactrix-markjohn <chukwuemekaokparaeke@gmail.com>
2024-10-31 14:09:14 +0100

    My Fleet screen finished

--- Robinson Helicopter/ViewModels/MyFleetItem.swift
+++ Robinson Helicopter/ViewModels/MyFleetItem.swift
@@ -20,99 +20,161 @@ struct MyFleetItem: View {
         
         ZStack {
             
-            RoundedRectangle(cornerRadius: 8)
+            /*RoundedRectangle(cornerRadius: 8)
                 .fill(Color("Cream"))
                 .stroke(Color("White"), lineWidth: 1)
-                .shadow(color: Color(white: 0,opacity:0.04),radius: 8)
+                .shadow(color: Color(white: 0,opacity:0.04),radius: 8)*/
             
-            HStack (spacing: 0) {
-                
-                Image(heliimage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 116)
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            VStack {
                 
                 
-                
-                
-                HStack {
+                HStack (spacing: 0) {
                     
-                    VStack (alignment: .leading) {
+                    Image(heliimage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 116)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    
+                    
+                    
+                    
+                    HStack {
                         
                         VStack (alignment: .leading) {
                             
-                            Text(title)
-                                .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 16))
-                                .foregroundStyle(Color("Black"))
+                            VStack (alignment: .leading) {
+                                
+                                Text(title)
+                                    .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 16))
+                                    .foregroundStyle(Color("Black"))
+                                
+                                Text(subtitle1)
+                                    .font(Font.custom(StringHolder.FontInter28ptLight, size: 12))
+                                    .foregroundStyle(Color("Black"))
+                                
+                                Text(subtitle2)
+                                    .font(Font.custom(StringHolder.FontInter28ptLight, size: 12))
+                                    .foregroundStyle(Color("Black"))
+                                
+                            }
                             
-                            Text(subtitle1)
-                                .font(Font.custom(StringHolder.FontInter28ptLight, size: 12))
-                                .foregroundStyle(Color("Black"))
                             
-                            Text(subtitle2)
-                                .font(Font.custom(StringHolder.FontInter28ptLight, size: 12))
-                                .foregroundStyle(Color("Black"))
+                            HStack {
+                                
+                                Button(action: {
+                                    // action
+                                }, label: {
+                                    
+                                    
+                                    HStack {
+                                        
+                                        Text("Document")
+                                            .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 14))
+                                            .foregroundStyle(Color("Blue"))
+                                        
+                                        Image("cheveron-right")
+                                            .resizable()
+                                            .renderingMode(.template) // Set rendering mode to template
+                                            .foregroundColor(Color("Blue"))
+                                        .frame(width: 14, height: 14)
+                                    }
+                                    
+                                })
+                                
+                                Spacer()
+                            }
+                            
+                            Spacer()
                             
                         }
                         
                         
-                        HStack {
-                            
+                        VStack {
                             Button(action: {
                                 // action
                             }, label: {
-                                
-                                
-                                HStack {
-                                    
-                                    Text("Document")
-                                        .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 14))
-                                        .foregroundStyle(Color("Blue"))
-                                    
-                                    Image("cheveron-right")
-                                        .resizable()
-                                        .renderingMode(.template) // Set rendering mode to template
-                                        .foregroundColor(Color("Blue"))
-                                    .frame(width: 14, height: 14)
-                                }
+                                Image("dots-vertical")
+                                    .resizable()
+                                    .renderingMode(.template) // Set rendering mode to template
+                                    .foregroundColor(Color("Blue"))
+                                    .frame(width: 20, height: 20)
                                 
                             })
                             
                             Spacer()
                         }
                         
-                        Spacer()
-                        
                     }
+                    .frame(width: .infinity)
+                    .padding(.vertical,12)
                     
-                    
-                    VStack {
-                        Button(action: {
-                            // action
-                        }, label: {
-                            Image("dots-vertical")
+                    Spacer()
+                }.frame(height: 116)
+                
+                
+                // The Safety Alert
+                ZStack {
+                    ZStack{
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color("LightCream"))
+                            
+                            .shadow(color: Color(white: 0,opacity:0.05),radius: 8)
+                        
+                        HStack {
+                            
+                            Image("safetyavatar")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                            
+                            VStack (alignment: .leading) {
+                                Text("Safety Alert")
+                                    .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 12))
+                                    .foregroundStyle(Color("Blue"))
+                                
+                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing\nelit, sed do eiusmod tempor.")
+                                    .font(Font.custom(StringHolder.FontInter28ptRegular, size: 10))
+                                    .foregroundStyle(Color("LightGrey"))
+                                
+                            }
+                            .padding(.trailing,10)
+                            
+                            Spacer()
+                            
+                            Image("cheveron-right")
                                 .resizable()
                                 .renderingMode(.template) // Set rendering mode to template
                                 .foregroundColor(Color("Blue"))
-                                .frame(width: 20, height: 20)
+                            .frame(width: 14, height: 14)
                             
-                        })
+                        }
+                        .padding(.horizontal,10)
                         
-                        Spacer()
                     }
-                    
-                }
-                .frame(width: .infinity)
-                .padding(.vertical,12)
+                    .frame(height: 68)
                 
-                Spacer()
+                }.padding(.horizontal, 8)
+                    .padding(.bottom,8)
+                
             }
             
-        }.frame(height: 116)
+        }
+        .background(Color("Cream"))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .overlay(content: {
+            RoundedRectangle(cornerRadius: 8)
+                
+                .stroke(Color("White"), lineWidth: 1)
+                .shadow(color: Color(white: 0,opacity:0.04),radius: 8)
+        })
+        
         
         
     }
+    
+    
 }
 
 #Preview {
--- Robinson Helicopter/Views/MyFleetView.swift
+++ Robinson Helicopter/Views/MyFleetView.swift
@@ -24,11 +24,13 @@ struct MyFleetView: View {
                 // Other contents
                 ZStack {
                     // First view
-                    //AddFleetView
+                    AddFleetView
+                        .opacity(0)
                     
                     // Second view (Main view)
                     
                     MyFleetMainView
+                        .opacity(1)
                     
                 }
                 .padding(.horizontal,16)
@@ -50,10 +52,27 @@ struct MyFleetView: View {
                 TrackFleetView
                     .padding(16)
                 
-                //InfoFindSerialNum
+                InfoFindSerialNum
+                    .opacity(0)
                 
             }
             .opacity(0)
+            
+            
+            
+            // Reorder My Fleet
+            ReorderMyFleetCard
+                .opacity(0)
+            
+            RemoveDealerConfirmation
+                .opacity(0)
+            
+           
+            SafetyAlertDetails
+                .opacity(0)
+            
+            SafetyAlertDetailsFullScreen
+                .opacity(0)
             
             
         }
@@ -382,6 +401,297 @@ struct MyFleetView: View {
             
         }
     }
+    
+    @ViewBuilder
+    var ReorderMyFleetCard: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("Black"))
+                .opacity(0.4)
+                .ignoresSafeArea()
+            
+            ZStack {
+                
+                
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("Cream"))
+                
+                VStack(spacing: 0) {
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            // action
+                        }, label: {
+                            Image("x")
+                                .resizable()
+                                .renderingMode(.template) // Set rendering mode to template
+                                .foregroundColor(Color("Blue"))
+                                .frame(width: 24, height: 24)
+                            
+                        })
+                    }
+                    
+                    HStack {
+                        
+                        VStack (alignment: .leading, spacing: -5) {
+                            Text("My Fleet")
+                                .foregroundStyle(Color("LightGrey"))
+                                .font(Font.custom(StringHolder.FontInter28ptRegular, size: 12))
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom,12)
+                            Text("Reorder Content")
+                                .foregroundStyle(Color("Black"))
+                                .font(Font.custom("Inter28pt-SemiBold", size: 16))
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom,12)
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    
+//                    List {
+//                        
+//                        
+                    //                    }
+                    
+                    
+                    VStack (spacing: 15) {
+                        
+                        // List of Fleet Item
+                        
+                        ReorderFleetItem(heliimage: "heliplaceholder", title: "R66 Turbine Marine", subtitle1: "D89 - T56 - H64", subtitle2: "N788AJ")
+                        
+                        ReorderFleetItem(heliimage: "heliplaceholderwhite", title: "R44 Cadet", subtitle1: "D89 - T56 - H64", subtitle2: "N788AJ")
+                        
+                        ReorderFleetItem(heliimage: "heliplaceholderblack", title: "R44 Raven II", subtitle1: "D89 - T56 - H64", subtitle2: "N788AJ")
+                        
+                        
+                    }
+                    .padding(.bottom,23)
+                    
+                    
+                    Spacer()
+                
+                    
+                    HStack {
+                        
+                        Spacer()
+                        CustomButton(title: "Cancel", icon: "", backcolor: Color("Cream"), textcolor: Color("Blue"), bordercolor: Color("Cream")) {
+                            // actiomn
+                        }
+                        .frame(width: 100,height: 40)
+                        
+                        CustomButton(title: "Save new order", icon: "", backcolor: Color("StrangeBlue"), textcolor: Color("Blue"), bordercolor: Color("StrangeBlue")) {
+                            // actiomn
+                        }
+                        .frame(width: 140,height: 40)
+                    }
+                    
+                    
+                    
+                }
+                .padding(24)
+                
+                   
+                
+            }
+            .frame(width:360,height: 439)
+            
+            
+        }
+    }
+    
+    @ViewBuilder
+    var RemoveDealerConfirmation: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("Black"))
+                .opacity(0.4)
+                .ignoresSafeArea()
+            
+            ZStack {
+                
+                
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("Cream"))
+                
+                VStack {
+                    
+                    Text("Are you sure you want to delete\nR66 Turbine Marine from your\nfleet?")
+                        .foregroundStyle(Color("Black"))
+                        .font(Font.custom("Inter28pt-SemiBold", size: 14))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom,12)
+                
+                    
+                    HStack {
+                        CustomButton(title: "Yes", icon: "", backcolor: Color("Cream"), textcolor: Color("Blue"), bordercolor: Color("Cream")) {
+                            // actiomn
+                        }
+                        
+                        CustomButton(title: "No", icon: "", backcolor: Color("Yellow"), textcolor: Color("Blue"), bordercolor: Color("Yellow")) {
+                            // actiomn
+                        }
+                    }
+                    
+                }
+                .padding(15)
+                
+                   
+                
+            }
+            .frame(width:266,height: 146)
+            
+            
+        }
+    }
+    
+    @ViewBuilder
+    var SafetyAlertDetails: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("Black"))
+                .opacity(0.4)
+                .ignoresSafeArea()
+            
+            ZStack {
+                
+                
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color("White"))
+                
+                VStack(spacing: 0) {
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            // action
+                        }, label: {
+                            Image("x")
+                                .resizable()
+                                .renderingMode(.template) // Set rendering mode to template
+                                .foregroundColor(Color("Blue"))
+                                .frame(width: 24, height: 24)
+                            
+                        })
+                    }
+                    .padding(.bottom,16)
+                    
+                    ZStack {
+                        
+                        Image("SafetyNotice")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 312, height: 401)
+                        
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Button(action: {
+                                    // action
+                                }
+                                       , label: {
+                                    CustomCircleImage(icon: "maximize screen", iconcolor: Color("Cream"),circlecolor: Color("Blue"), circlewidth: 40, circleheight: 40)
+                                })
+                            }
+                            .padding(.bottom,15)
+                        }
+                        
+                        
+                    }.frame(width: 312, height: 401)
+                        .padding(.bottom,16)
+                    
+                    HStack {
+                        
+                        VStack (alignment: .leading) {
+                            
+                            Text("This document is saved in")
+                                .foregroundStyle(Color("Black"))
+                                .font(Font.custom(StringHolder.FontInter28ptRegular, size: 12))
+                            
+                            Text("Menu > Documents > Safety alerts")
+                                .foregroundStyle(Color("Black"))
+                                .font(Font.custom(StringHolder.FontInter28ptSemiBold, size: 12))
+                                
+                            
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            CustomCircleImage(icon: "download", circlecolor: Color("Yellow"), circlewidth: 40, circleheight: 40)
+                        })
+                        
+                    }
+                    
+                    
+                    
+                    Spacer()
+                
+                    
+                    
+                }
+                .padding(24)
+                
+                   
+                
+            }
+            .frame(width:360,height: 547)
+            
+            
+        }
+    }
+    
+    @ViewBuilder
+    var SafetyAlertDetailsFullScreen: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("Black"))
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        // action
+                    }, label: {
+                        Image("x")
+                            .resizable()
+                            .renderingMode(.template) // Set rendering mode to template
+                            .foregroundColor(Color("Cream"))
+                            .frame(width: 24, height: 24)
+                        
+                    })
+                }
+                .padding(.bottom,16)
+                
+                Spacer()
+                
+                Image("SafetyNotice")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 360, height: 463)
+                
+                Spacer()
+                
+            }.padding(16)
+            
+            
+            
+            
+        }
+    }
+
+
 }
 
 #Preview {
